ded0b4e3699fa6a35485e5b210584508e9f0a3b8
Yurii Shevtsov <yurii.shevtsov@do-it.co>
2020-04-24 15:43:33 +0300

    Add unit tests for some parts of CoursesModule

--- src/app/courses/course-search/course-search.component.spec.ts
+++ src/app/courses/course-search/course-search.component.spec.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseSearchComponent } from './course-search.component';
@@ -8,7 +9,9 @@ describe('CourseSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseSearchComponent ]
+      declarations: [ CourseSearchComponent ],
+      imports: [FormsModule, ReactiveFormsModule],
+      providers: [FormBuilder]
     })
     .compileComponents();
   }));
@@ -22,4 +25,15 @@ describe('CourseSearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit queryChange', () => {
+    jasmine.clock().install()
+    const queryChangeSpy = spyOn<any>(component.queryChange, 'emit').and.callThrough();
+    component.ngOnInit();
+    return fixture.whenStable().then(() => {
+      component.searchForm.get('query').setValue('foo');
+      jasmine.clock().tick(500);
+      return expect(queryChangeSpy).toHaveBeenCalled();
+    });
+  })
 });
--- src/app/courses/courses.service.spec.ts
+++ src/app/courses/courses.service.spec.ts
@@ -1,16 +1,82 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { CoursesService } from './courses.service';
+import { Course } from "./types/course";
+import { environment } from '../../environments/environment'
 
-describe('CoursesService', () => {
+const baseUrl = environment.baseUrl;
+
+const coursesMock: Array<Course> = [
+  {
+    "id": 1,
+    "title": "Modern Web Development",
+    "duration": 5,
+    "duration-unit": "day",
+    "description": "HTML5, CSS3, ES6/JS and more"
+  },
+  {
+    "id": 2,
+    "title": "Git",
+    "duration": 3,
+    "duration-unit": "day",
+    "description": "Git and GitHub"
+  },
+  {
+    "id": 3,
+    "title": "Python",
+    "duration": 5,
+    "duration-unit": "day",
+    "description": "Intro to Python"
+  }
+];
+describe('CourseService', () => {
   let service: CoursesService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(CoursesService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get courses', () => {
+    service.getCourses().subscribe((data) => {
+      expect(data).toBe(coursesMock);
+      expect(data.length).toEqual(3);
+    });
+
+    let coursesRequest = httpMock.expectOne(`${baseUrl}/courses?q=`);
+    coursesRequest.flush(coursesMock);
+
+    httpMock.verify();
+  });
+
+  it('should create course', () => {
+    const freshThirdCourse = Object.assign({}, coursesMock[2], { id: undefined });
+    const freshThirdCourseWithId = Object.assign({}, coursesMock[2]);
+
+    service.createCourse(freshThirdCourse).subscribe((data) => {
+      expect(data).toBe(freshThirdCourseWithId);
+    });
+
+    let createCourseRequest = httpMock.expectOne(`${baseUrl}/courses`);
+    createCourseRequest.flush(freshThirdCourseWithId);
+
+    expect(createCourseRequest.request.method).toBe('POST');
+
+    httpMock.verify();
+  });
+
+  /*
+   TODO: This is not a complete unit-test of CoursesService, due to time constraints.
+     Also other service methods would be tested in exactly the same manner.
+      So, I hope, this sample is enough to evaluate my unit-testing skills.
+  */
 });
